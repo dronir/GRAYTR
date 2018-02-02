e47eb4357be0d713992ee71007e9941cf7efba55
Olli Wilkman <olli.wilkman@iki.fi>
2018-02-02 02:28:02 +0200

    Add directional light

--- src/lights.jl
+++ src/lights.jl
@@ -1,6 +1,10 @@
 
 abstract type LightSource end
 
+
+################################
+# Visibility Tester
+
 struct VisibilityTester
     ray::Ray
 end
@@ -19,12 +23,20 @@ end
 unoccluded(V::VisibilityTester, scene::Scene) = !intersectP(V.ray, scene)
 
 
-struct PointLight
+################################
+# Point light
+
+struct PointLight <: LightSource
     position::Point3
     intensity::Spectrum
     light_to_world::Transformation
     world_to_light::Transformation
     nsamples::Int64
+end
+
+# Minimal constructor
+function PointLight(L::Spectrum, l2w::Transformation, nsamples::Int64)
+    PointLight(l2w(Point3(0)), L, l2w, inv(l2w), nsamples)
 end
 
 isdelta(L::PointLight) = true
@@ -35,7 +47,27 @@ function sample_L(light::PointLight, p::Point3)
     L = light.intensity / distance^2
     wi = (light.position - p) / distance
     pdf = 1.0
-    vis = VisibilityTester(p, light.position, pEps, 0.0)
+    vis = VisibilityTester(p, light.position, 2e-5, 0.0)
     return L, wi, pdf, vis
+end
+
+
+################################
+# Distance light source
+
+struct DistantLight <: LightSource
+    direction::Vector3
+    intensity::Spectrum
+    light_to_world::Transformation
+    world_to_light::Transformation
+    nsamples::Int64
+end
+
+function DistantLight(dir::Vector3, L::Spectrum, l2w::Transformation, nsamples::Int64)
+    DistantLight(normalize(l2w(dir)), L, l2w, inv(l2w), nsamples)
+end
+
+function sample_L(light::DistantLight, p::Point3)
+    return light.L, light.direction, 1.0, VisibilityTester(p, light.direction, 2e-5)
 end
 
