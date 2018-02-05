53071c5c6d5b0e851bd63ede7fdfbba9c0258d35
Olli Wilkman <olli.wilkman@iki.fi>
2018-02-05 15:09:08 +0200

    Integrator.jl added with Whitted integrator

---
+++ src/integrator.jl
@@ -0,0 +1,41 @@
+
+abstract type Integrator end
+abstract type SurfaceIntegrator <: Integrator end
+abstract type VolumeIntegrator <: Integrator end
+
+
+struct WhittedIntegrator <: SurfaceIntegrator
+    maxdepth::Int64
+end
+
+function intensity(intgr::WhittedIntegrator, rend::Renderer, scene::Scene, isect::Intersection, 
+                   r::Ray, sample::Sample)
+    # evaluate BSDF at intersect point
+    L = 0.0
+    bsdf = get_BSDF(isect, ray)
+    p = bsdf.dgshading.p
+    n = bsdf.dgshading.n
+    wo = -r.direction
+
+    # compute emitted light if hit object is an area light
+    L += emission(iSect, w0)
+    
+    # add contribution of each light source
+    for light in scene.lights
+        Li, wi, pdf, vis = sample_light(light)
+        if isblack(Li) || pdf ≈ 0.0
+            continue
+        end
+        f = bsdf(wi, wo)
+        if !isblack(f) && !occluded(vis)
+            L += f .* Li * abs(dot(wi, n))
+        end
+    end
+    return L
+    
+    # TODO: recursive for depth
+#    if ray.depth < maxDepth
+        # trace rays for transmission and reflection
+#    end
+end
+
