fb562a65192263ddb994407fe956e15dcc1cd344
Olli Wilkman <olli.wilkman@iki.fi>
2018-02-06 02:31:59 +0200

    Some comments

--- src/abstracts.jl
+++ src/abstracts.jl
@@ -1,9 +1,20 @@
+# All the abstract types are declared in this file. Some type definitions depend
+# on various abstract types in ways that make keeping each abstract type in the
+# most relevant file impossible. 
+
+# E.g. funcions for Scene depends on LightSource and Primitive, Primitive
+# depends on AreaLight, and functions for light sources depend on Scene.
+
+# For primitives.jl
 abstract type Primitive end
 
+# For bsdr.jl
 abstract type BxDF end
 
+# For shape.jl
 abstract type Shape end
 
+# For materials.jl
 abstract type Material end
 
 # For spectrum.jl
@@ -22,5 +33,6 @@ abstract type Integrator end
 abstract type SurfaceIntegrator <: Integrator end
 abstract type VolumeIntegrator <: Integrator end
 
+# For lights.jl
 abstract type LightSource end
 
