7afed40c6faa850f5e91e52d612487737b2d3c0a
Olli Wilkman <olli.wilkman@iki.fi>
2018-02-20 18:06:46 +0200

    Update Pixel for slight performance boost

--- src/cameras.jl
+++ src/cameras.jl
@@ -11,7 +11,9 @@ end
 
 import Base.zero
 zero(::Type{Pixel}) = Pixel(0.0,0.0,0.0,0.0)
+add(P::Pixel, S::Array{Float64,1}) = Pixel(P.x+S[1], P.y+S[2], P.z+S[3], P.w+S[4])
 add(P::Pixel, S::Array{Float64,1}, w::Float64) = Pixel(P.x+S[1], P.y+S[2], P.z+S[3], P.w+w)
+add(P::Pixel, x, y, z, w) = Pixel(P.x+x, P.y+y, P.z+z, P.w+w)
 
 include("films/imagefilm.jl")
 include("films/delayfilm.jl")
--- src/films/imagefilm.jl
+++ src/films/imagefilm.jl
@@ -44,7 +44,7 @@ function add_sample!(F::ImageFilm, sample::Sample, L::Spectrum)
         for j = y0:y1
             # TODO: unoptimized way, without using filter table
             w = evaluate(F.filter, (i-dimgX), (j-dimgY))
-            F.pixels[i,j] = add(F.pixels[i,j], w*xyz, w)
+            F.pixels[i,j] = add(F.pixels[i,j], w*xyz[1], w*xyz[2], w*xyz[3], w)
         end
     end
 end
