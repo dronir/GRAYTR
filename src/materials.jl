9b501f225c73a5cc72308cf79ef504b825d70992
Olli Wilkman <olli.wilkman@iki.fi>
2018-02-20 17:57:20 +0200

    Simplifications in materials

--- src/materials.jl
+++ src/materials.jl
@@ -8,12 +8,12 @@ struct BSDF
     nn::Normal3
     sn::Vector3
     tn::Vector3
-    BxDFlist::Array{BxDF,1}
+    local_BxDF::BxDF
     refidx::Float64
 end
 
 
-function BSDF(dgs::DifferentialGeometry, ng::Normal3, L::Array{<:BxDF,1}, e::Float64)
+function BSDF(dgs::DifferentialGeometry, ng::Normal3, L::BxDF, e::Float64)
     nn = dgs.n
     sn = normalize(dgs.dpdu)
     tn = cross(nn,sn)
@@ -22,11 +22,11 @@ end
 
 BSDF(dgs::DifferentialGeometry, ng::Normal3, L::Array{BxDF,1}) = BSDF(dgs,ng,L,1.0)
 
-ncomponents(B::BSDF) = size(B.BxDFlist)
+#ncomponents(B::BSDF) = size(B.BxDFlist)
 world_to_local(B::BSDF, v::Vector3) = Vector3(dot(v, B.sn), dot(v, B.tn), dot(v, B.nn))
-local_to_world(B::BSDF, v::Vector3) = Vector3(sn.x*v.x + tn.x*v.y + nn.x*vz,
-                                              sn.y*v.x + tn.y*v.y + nn.y*vz,
-                                              sn.z*v.x + tn.z*v.y + nn.z*vz)
+local_to_world(B::BSDF, v::Vector3) = Vector3(B.sn.x*v.x + B.tn.x*v.y + B.nn.x*vz,
+                                              B.sn.y*v.x + B.tn.y*v.y + B.nn.y*vz,
+                                              B.sn.z*v.x + B.tn.z*v.y + B.nn.z*vz)
 
 function evaluate(B::BSDF, w0::Vector3, w1::Vector3)
     w0l = world_to_local(B, w0)
@@ -37,16 +37,18 @@ function evaluate(B::BSDF, w0::Vector3, w1::Vector3)
     else
         flags = flags & ~BSDF_REFLECTION
     end
-    return sum(evaluate(some_BRDF, w0l, w1l) for some_BRDF in B.BxDFlist)
+    return evaluate(B.local_BxDF, w0l, w1l)
 end
 
 
 # The simplest material, just Lambertian
 
 struct MatteMaterial <: Material
-    Kd::Spectrum
+    B::BxDF
 end
 
+MatteMaterial(Kd::Spectrum) = MatteMaterial(Lambert(Kd))
+
 function get_BSDF(M::MatteMaterial, dg::DifferentialGeometry, dgs::DifferentialGeometry)
-    return BSDF(dgs, dg.n, [Lambert(M.Kd)], 1.0)
+    return BSDF(dgs, dg.n, M.B, 1.0)
 end
