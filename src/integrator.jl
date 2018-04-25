057d0eb34485bc08d4f68d6233ff675ff569f685
Olli Wilkman <olli.wilkman@iki.fi>
2018-04-25 11:17:18 +0300

    Small optimization in integrator

--- src/integrator.jl
+++ src/integrator.jl
@@ -1,8 +1,8 @@
 
 # Dummy volume integrator for testing
 struct DummyVolumeIntegrator <: VolumeIntegrator end
-
 preprocess(D::DummyVolumeIntegrator) = true
+
 
 struct WhittedIntegrator <: SurfaceIntegrator
     maxdepth::Int64
@@ -10,12 +10,7 @@ end
 
 preprocess(W::WhittedIntegrator) = true
 
-+(x::Void, y::Spectrum) = y
-
 function inner_int(light::LightSource, p::Point3, bsdf, wo::Vector3, n::Normal3, scene::Scene)
-    if !direct(light)
-        return NoLight()
-    end
     Li, wi, pdf, vis = sample_L(light, p)
     if isblack(Li) || pdf ≈ 0.0
         return NoLight()
@@ -42,6 +37,7 @@ function intensity(intgr::WhittedIntegrator, rend::Renderer, scene::Scene, isect
     # add contribution of each light source
     L = NoLight()
     for light in scene.lights
+        !direct(light) && continue
         L += inner_int(light, p, bsdf, wo, n, scene) 
     end
     return L
