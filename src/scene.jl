e3e2aee4848643f6312b08c75f668d16131897a5
Olli Wilkman <olli.wilkman@iki.fi>
2018-04-25 11:17:38 +0300

    Parametrize scene by aggregate instead of primitive

--- src/scene.jl
+++ src/scene.jl
@@ -2,13 +2,13 @@
 # The Scene type contains the root node of the object hierarchy, a list of the light sources
 # in the scene, and the global bounding box.
 
-struct Scene{T<:Primitive}
+struct Scene{T<:Aggregate}
     aggregate::T
     lights::Array{LightSource,1}
     bounds::BoundingBox
 end
 
-Scene(agg::Primitive, lights::Array{LightSource,1}) = Scene(agg, lights, world_bounds(agg))
+Scene(agg::Aggregate, lights::Array{LightSource,1}) = Scene(agg, lights, world_bounds(agg))
 
 world_bounds(S::Scene) = Scene.bounds
 intersect(r::Ray, S::Scene) = intersect(r, S.aggregate)
