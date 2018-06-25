6180e7569ef174fe92efce75bc57f494ce7562e4
Olli Wilkman <olli.wilkman@iki.fi>
2018-06-25 17:59:13 +0300

    Generalize Ray(t) from Float64 to Real

--- src/rays.jl
+++ src/rays.jl
@@ -12,7 +12,7 @@ end
 Ray(origin::Point3, direction::Vector3) = Ray(origin, direction, 0.0, Inf, 1)
 Ray(parent::Ray, origin::Point3, direction::Vector3) = Ray(origin, direction, 0.0, Inf, parent.depth+1)
 
-(R::Ray)(t::Float64) = R.origin + t*R.direction
+(R::Ray)(t::Real) = R.origin + t*R.direction
 
 function (transform::Transformation)(R::Ray)
     return Ray(transform(R.origin), transform(R.direction), R.tmin, R.tmax, R.depth)
