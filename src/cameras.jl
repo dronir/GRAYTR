137dac8f9594a4707f34004a977784597d5b9bd2
Olli Wilkman <olli.wilkman@iki.fi>
2018-07-11 12:46:52 +0300

    Don't auto-import films in cameras.jl

--- src/cameras.jl
+++ src/cameras.jl
@@ -15,6 +15,4 @@ add(P::Pixel, S::Array{Float64,1}) = Pixel(P.x+S[1], P.y+S[2], P.z+S[3], P.w+S[4
 add(P::Pixel, S::Array{Float64,1}, w::Float64) = Pixel(P.x+S[1], P.y+S[2], P.z+S[3], P.w+w)
 add(P::Pixel, x, y, z, w) = Pixel(P.x+x, P.y+y, P.z+z, P.w+w)
 
-include("films/imagefilm.jl")
-include("films/delayfilm.jl")
 
