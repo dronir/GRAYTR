fe39436ba9b24cccb1fc6db5fcbb54444e63e102
Olli Wilkman <olli.wilkman@iki.fi>
2018-09-28 15:14:23 +0300

    Add docstrings to utility.jl

--- src/utility.jl
+++ src/utility.jl
@@ -1,10 +1,21 @@
 
-# Linear extrapolation/interpolation
+"""
+    lerp(t, a, b)
+
+Linear interpolation on the interval `[a, b]` as `t ∈ [0, 1].
+"""
 lerp(t, a, b) = (one(t)-t)*a + t*b
 
 
-# Solve quadratic equation. Needed by different shapes' intersection calculation.
-function quadratic(A, B, C)
+"""
+    quadratic(A, B, C)
+
+Solve quadratic equation `A x^2 + B x + C = 0` for real coefficients.
+
+If a real solution exists, returns `(true, t0, t1)` where `t0` is the smaller
+and `t1` the larger solution. If no real solution exists, returns `(false, Inf, Inf)`.
+"""
+function quadratic(A::Real, B::Real, C::Real)
     dd = B^2 - 4*A*C
     if dd < 0.0
         return false, Inf, Inf
@@ -15,9 +26,14 @@ function quadratic(A, B, C)
     return true, min(t0,t1), max(t0,t1)
 end
 
-# Compute partial derivatives of normal vectors from Weingarten equations,
-# given first and second derivatives of position vector.
-# Used by several shape models.
+
+"""
+    normal_derivatives(dpdu, dpdv, dpduu, dpduv, dpdvv)
+
+Compute partial derivatives of normal vectors from Weingarten equations,
+given first and second derivatives of position vector.
+Used by several shape models.
+"""
 function normal_derivatives(dpdu, dpdv, dpduu, dpduv, dpdvv)
     E = dot(dpdu, dpdu)
     F = dot(dpdu, dpdv)
