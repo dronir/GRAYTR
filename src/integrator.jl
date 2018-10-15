42b2c4145999dab282a7213a7d3c2d7771c901b8
Olli Wilkman <olli.wilkman@iki.fi>
2018-10-15 15:16:31 +0300

    Docstrings in integrator and samplers

--- src/integrator.jl
+++ src/integrator.jl
@@ -1,30 +1,57 @@
 
-# Dummy volume integrator for testing
+
+"""
+    DummyVolumeIntegrator
+
+A VolumeIntegrator that does nothing, for cases where one is not functionally needed.
+Currently the only VolumeIntegrator.
+"""
 struct DummyVolumeIntegrator <: VolumeIntegrator end
+
+
+"""
+    preprocess(D::DummyVolumeIntegrator)
+
+Dummy integrator requires no preprocessing so this just returns true.
+""" 
 preprocess(D::DummyVolumeIntegrator) = true
 
 
+
+"""
+    WhittedIntegrator
+
+The standard (and currently only) SurfaceIntegrator. Traces rays from surface point to light
+sources and adds together their contributions. Recursion to ray depths greater than one is not
+currently implemented yet.
+"""
 struct WhittedIntegrator <: SurfaceIntegrator
+    "maxdepth, the maximum ray depth to trace recursively."
     maxdepth::Int64
 end
 
+
+"""
+    preprocess(W::WhittedIntegrator)
+
+Whitted integrator requires no preprocessing so this just returns true.
+""" 
 preprocess(W::WhittedIntegrator) = true
 
-# This inner part of loop separated so that it can be dispatched based on the type of 'light':
-function inner_int(light::LightSource, dg::DifferentialGeometry, mat::BxDF, w1::Vector3, T::Transformation, scene::Scene)
-    !direct(light) && return nolight
-    light_spectrum, w0, pdf, light_ray = sample_L(light, dg.p)
-    if isblack(light_spectrum) || pdf ≈ 0.0
-        return nolight
-    end
-    if intersectP(light_ray, scene)
-        return nolight
-    else
-        brdf_value = evaluate(mat, T, w1, w0)
-        return (brdf_value * light_spectrum) * abs(dot(w0, dg.n))
-    end
-end
 
+"""
+    intensity(integrator, scene, isect, ray, sample)
+
+Return intensity given a WhittedIntegrator, a scene, intersection point, ray and camera
+sample. 
+
+# Arguments
+- `integrator::WhittedIntegrator`: a WhittedIntegrator instance.
+- `scene::Scene`: the Scene to trace illumination rays in.
+- `isect::Intersection`: an Intersection, giving the local geometry at ray intersection.
+- `ray::Ray`, the incident Ray (not used by this function but part of SurfaceIntegrator API)
+- `sample::Sample`, a Sample (not used by this function but part of SurfaceIntegrator API)
+"""
 function intensity(intgr::WhittedIntegrator, scene::Scene, isect::Intersection, 
                    ray::Ray, sample::Sample)
     # evaluate BSDF at intersect point
@@ -45,5 +72,26 @@ function intensity(intgr::WhittedIntegrator, scene::Scene, isect::Intersection,
         # trace rays for transmission and reflection
 #    end
 end
+
+"""
+    inner_int(light::LightSource, dg::DifferentialGeometry, 
+                 mat::BxDF, w1::Vector3, T::Transformation, scene::Scene)
+                 
+The inner loop of the `intensity` function, separated for better optimization."""
+function inner_int(light::LightSource, dg::DifferentialGeometry, mat::BxDF, w1::Vector3, T::Transformation, scene::Scene)
+    !direct(light) && return nolight
+    light_spectrum, w0, pdf, light_ray = sample_L(light, dg.p)
+    if isblack(light_spectrum) || pdf ≈ 0.0
+        return nolight
+    end
+    if intersectP(light_ray, scene)
+        return nolight
+    else
+        brdf_value = evaluate(mat, T, w1, w0)
+        return (brdf_value * light_spectrum) * abs(dot(w0, dg.n))
+    end
+end
+
+
 
 
--- src/samplers.jl
+++ src/samplers.jl
@@ -7,10 +7,33 @@
 
 
 
-# Stratified1D and Stratified2D generate a bunch of stratified samples in the [0,1] interval.
 
-stratified1D(N::Integer, jitter::Bool) = [(i-1 + (jitter ? rand() : 0.5))/N for i = 0:N:1]
+"""
+    stratified1D(N::Integer, jitter::Bool)
 
+Divides the `[0,1]` interval into `N` bins and either returns an array with the bin centers 
+(if `jitter == false`), or a uniformly random point inside each bin (if `jitter == true`).
+
+# Examples
+```julia-repl
+julia> stratified1D(2, false)
+2-element Array{Float64,1}:
+ 0.25
+ 0.75
+```
+"""
+stratified1D(N::Integer, jitter::Bool) = [(i-1 + (jitter ? rand() : 0.5))/N for i = 1:N]
+
+
+"""
+    stratified2D(N::Integer, jitter::Bool)
+
+Divides the unit square into `N` by `N` square bins either returns the bin centers (if
+`jitter == false`), or a random point inside each bin (if `jitter == true`).
+
+The output is an `(N, 2)` shaped array.
+
+"""
 function stratified2D(N::Integer, jitter::Bool)
     out = zeros(Float64, (N,2))
     for i = 1:N
@@ -20,9 +43,18 @@ function stratified2D(N::Integer, jitter::Bool)
     out
 end
 
-# This computes the pixel range which a given sampler samples, given
-# the number `n` of the area and the total number `count` of areas.
-# Note that indexing starts from 1 unlike in book's C++ code.
+
+"""
+    compute_subwindow(sampler::Sampler, n::Int64, count::Int64)
+
+This computes the pixel range which a given subsampler samples, given the index `n` of the
+subsampler and the total number of areas, `count`.
+
+Returns four integers, given the pixel coordinates (x0, x1, y0, y1) of the subwindow
+(inclusive).
+
+Note that indexing starts from 1 unlike in book's C++ code.
+"""
 function compute_subwindow(sampler::Sampler, n::Int64, count::Int64)
     dx = sampler.xend - sampler.xstart + 1
     dy = sampler.yend - sampler.ystart + 1
@@ -43,7 +75,12 @@ function compute_subwindow(sampler::Sampler, n::Int64, count::Int64)
 end
 
 
+"""
+    CameraSample
 
+A wrapper for four floating point values, generally to represent two random points needed
+for ray creation from a camera: one point on the film and the other on the lens.
+"""
 struct CameraSample <: Sample
     imgX::Float64
     imgY::Float64
@@ -64,11 +101,16 @@ struct StratifiedSampler <: Sampler
     jitter::Bool
 end
 
-StratifiedSampler(x0::Int64, x1::Int64, y0::Int64, y1::Int64, xs::Int64, ys::Int64, j::Bool) = StratifiedSampler(x0,x1,y0,y1,xs,ys,xs*ys,j)
+StratifiedSampler(x0::Int64, x1::Int64, y0::Int64, y1::Int64, xs::Int64, ys::Int64, 
+                  j::Bool) = StratifiedSampler(x0,x1,y0,y1,xs,ys,xs*ys,j)
 
-roundsize(S::StratifiedSampler, size::Integer) = size
 
+"""
+    get_subsampler(sampler::StratifiedSampler, n::Integer, count::Integer)
 
+Given a `StratifiedSampler`, a total number of subwindows `count` and a subwindow index
+`n`, returns a new `StratifiedSampler` which samples that subwindow.
+"""
 function get_subsampler(sampler::StratifiedSampler, n::Integer, count::Integer)
     x0, x1, y0, y1 = compute_subwindow(sampler, n, count)
     if x0 == x1 || y0 == y1
@@ -78,6 +120,13 @@ function get_subsampler(sampler::StratifiedSampler, n::Integer, count::Integer)
     end
 end
 
+
+"""
+    finished(sampler::StratifiedSampler, state::Integer)
+
+Returns `true` when `state` indicates that `sampler` is "finished", i.e. cannot return
+any more samples.
+"""
 function finished(sampler::StratifiedSampler, state::Integer)
     dx = sampler.xend - sampler.xstart + 1
     dy = sampler.yend - sampler.ystart + 1
@@ -112,11 +161,18 @@ function get_samples(sampler::StratifiedSampler, state::Integer)
     return out, state+1
 end
 
+
+"""
+    get_samples!(sampler::StratifiedSampler, state::Integer, out::Array{CameraSample,1})
+
+Generate samples from the `StratifiedSampler`, given its `state`, and fill the array
+`out` with them. Returns the updated state of the sampler.
+"""
 function get_samples!(sampler::StratifiedSampler, state::Integer, out::Array{CameraSample,1})
     dx = sampler.xend - sampler.xstart + 1
     dy = sampler.yend - sampler.ystart + 1
     if state >= dx*dy
-        return CameraSample[], state
+        return state
     end
 
     xpos = sampler.xstart + state % dx
@@ -127,13 +183,12 @@ function get_samples!(sampler::StratifiedSampler, state::Integer, out::Array{Cam
     shuffle!(lens_samples[:,1])
     shuffle!(lens_samples[:,2])
     for i = 1:N
-        s = CameraSample(
+        out[i] = CameraSample(
             img_samples[i,1] + xpos,
             img_samples[i,2] + ypos,
             lens_samples[i,1],
             lens_samples[i,2]
         )
-        out[i] = s
     end
     return state+1
 end
