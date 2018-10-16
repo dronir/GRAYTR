13b3998fe6c343269c83d4677bb1ddb7681e5700
Olli Wilkman <olli.wilkman@iki.fi>
2018-10-16 18:16:47 +0300

    Docstrings in photometricfilm (though it is WIP)

--- src/films/photometric_film.jl
+++ src/films/photometric_film.jl
@@ -1,4 +1,13 @@
 
+"""
+    PhotometricFilm
+
+The photometric film is a one-pixel detector, simply adding together all the intensity
+coming from the scene to the camera.
+
+TODO: This is really not in working condition, do not use it.
+
+"""
 mutable struct PhotometricFilm{S<:Spectrum} <: Film
     integral::S
     N::Int64
@@ -7,9 +16,18 @@ mutable struct PhotometricFilm{S<:Spectrum} <: Film
     resY::Int64
 end
 
-PhotometricFilm() = PhotometricFilm(nolight, 0)
-PhotometricFilm(res::Integer) = PhotometricFilm(nolight, 0, res, res)
 
+"""
+    add_sample!(F::PhotometricFilm, sample::Sample, L::Spectrum, isect::Union{Intersection,Nothing})
+
+Add a ray sample to the `ImageFilm`. The spectral intensity `L` will be added to the
+detector's single pixel. The ray intersection `isect` nor the camera sample `sample` are
+not used, but they are part of the `Film` API.
+
+TODO: This relies heavily on the addition of Spectrum types, which is a bit poorly defined
+currently.
+
+"""
 function add_sample!(F::PhotometricFilm, sample::Sample, L::Spectrum, isect::Union{Intersection,Nothing})
     F.N += 1
     isblack(L) && return nothing
@@ -17,8 +35,16 @@ function add_sample!(F::PhotometricFilm, sample::Sample, L::Spectrum, isect::Uni
     return nothing
 end
 
+
+"""
+    write_txt(F::PhotometricFilm, fname::String)
+
+Write the value of the single pixel of the `PhotometricFilm` into a file named `fname`.
+
+"""
 function write_txt(F::PhotometricFilm, fname::String)
-    S = F.area / F.N * F.integral
-    
+    S = F.area / F.N * F.integral    
+    f = open(fname, "w")
+    write(f, string(S))
     return nothing
 end
