3ee2f9c7e50529313d03d0b8ceb2e6ac51561f34
Olli Wilkman <olli.wilkman@iki.fi>
2019-01-24 14:47:14 +0100

    More brdf tests

--- test/test_brdf.jl
+++ test/test_brdf.jl
@@ -10,23 +10,34 @@
     @test GRAYTR.sinphi(w) ≈ 0.0
 end
 
+spec = GRAYTR.SingleLine(532.0, 1.0)
+w0 = normalize(Vector3(0, 0, 1))
+w1 = normalize(Vector3(1, 0, 1))
+
 @testset "Lambert" begin
-    R = GRAYTR.Lambert(GRAYTR.SingleLine(532.0, 1.0))
-    
-    w0 = normalize(Vector3(0, 0, 1))
-    w1 = normalize(Vector3(1, 0, 1))
+    R = GRAYTR.Lambert(spec)
     
     @test GRAYTR.BSDF_type(R) == 9
     @test GRAYTR.evaluate(R, w0, w1) == GRAYTR.SingleLine(532.0, 1) / π
-    
     @test GRAYTR.rho(R, w0) == GRAYTR.SingleLine(532.0, 1)
 end
 
 @testset "Lommel-Seeliger" begin
+    P(alpha) = 1.0
+    R = GRAYTR.LommelSeeliger(spec, P)
+    
+    @test GRAYTR.BSDF_type(R) == 9
+    @test GRAYTR.evaluate(R, w0, w1) == GRAYTR.SingleLine(532.0, 1) * 0.25 / (1.0 + sqrt(2)/2)
+    
+    
 end
 
 @testset "Ashkhmin-Shirley" begin
 end
-
+    R = GRAYTR.AshkhminShirleySingle(spec, 0.5, 100.0)
+    @test GRAYTR.BSDF_type(R) == 9
+    
+#    @test GRAYTR.evaluate(R, w0, w1) <: GRAYTR.Spectrum
+    
 end
 
