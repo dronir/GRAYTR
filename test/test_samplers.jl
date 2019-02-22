d68168290aa876623f48e9ee7582f2ef1c49eb2d
Olli Wilkman <olli.wilkman@iki.fi>
2019-02-22 14:02:28 +0200

    Update sampler tests

--- test/test_samplers.jl
+++ test/test_samplers.jl
@@ -2,12 +2,12 @@
 @testset "Samplers" begin
 
 @testset "CameraSample" begin
-    s = GRAYTR.CameraSample(4.0, 0.5, 1.5, 2.0)
+    s = GRAYTR.CameraSample(4.0, 0.5, 0.75, 1.0)
     t = normalize(s, 2.0, 0.5)
     @test t.imgX ≈ 2.00
     @test t.imgY ≈ 1.0
     @test t.lensU ≈ 0.75
-    @test t.lensV ≈ 4.0
+    @test t.lensV ≈ 1.0
 end
 
 @testset "StratifiedSampler" begin
@@ -30,6 +30,25 @@ end
         @test GRAYTR.compute_subwindow(S, 2, 4) == (17, 32, 1, 32)
         @test GRAYTR.compute_subwindow(S, 3, 4) == (1, 16, 33, 64)
         @test GRAYTR.compute_subwindow(S, 4, 4) == (17, 32, 33, 64)
+        
+        S = GRAYTR.StratifiedSampler(2, 2, 2)
+        @test GRAYTR.compute_subwindow(S, 1, 4) == (1, 1, 1, 1)
+        @test GRAYTR.compute_subwindow(S, 2, 4) == (2, 2, 1, 1)
+        @test GRAYTR.compute_subwindow(S, 3, 4) == (1, 1, 2, 2)
+        @test GRAYTR.compute_subwindow(S, 4, 4) == (2, 2, 2, 2)
+        
+        #S = GRAYTR.StratifiedSampler(2, 4, 2)
+        #@test GRAYTR.compute_subwindow(S, 1, 4) == (1, 1, 1, 2)
+        #@test GRAYTR.compute_subwindow(S, 2, 4) == (2, 2, 1, 2)
+        #@test GRAYTR.compute_subwindow(S, 3, 4) == (1, 1, 3, 4)
+        #@test GRAYTR.compute_subwindow(S, 4, 4) == (2, 2, 3, 4)
+        #
+        #S = GRAYTR.StratifiedSampler(4, 2, 2)
+        #@test GRAYTR.compute_subwindow(S, 1, 4) == (1, 2, 1, 1)
+        #@test GRAYTR.compute_subwindow(S, 2, 4) == (3, 4, 1, 1)
+        #@test GRAYTR.compute_subwindow(S, 3, 4) == (1, 2, 2, 2)
+        #@test GRAYTR.compute_subwindow(S, 4, 4) == (3, 4, 2, 2)
+        
     end
     
     @testset "Subsampler" begin
@@ -44,6 +63,7 @@ end
         @test Sub.jitter == true
         @test Sub.xnorm == 32
         @test Sub.ynorm == 64
+        
     end
     
     @testset "Finished state" begin
