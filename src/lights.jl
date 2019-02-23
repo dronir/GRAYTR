e1adfd22a5f17ef72e56452fcde0d89a7cd917ce
Olli Wilkman <olli.wilkman@iki.fi>
2019-02-23 23:32:48 +0200

    Add docstrings to lights.jl

--- src/lights.jl
+++ src/lights.jl
@@ -4,22 +4,52 @@
 ################################
 # Visibility Tester is a ray from point to light source
 
-# VisibilityTester between two points (with epsilons)
+"""
+    VisibilityTester(p1::Point3, p2::Point3, eps1::Float64, eps2::Float64)
+
+Returns a ray to test intersection on the line segment from `p1` to `p2` with the given ray
+epsilon values.
+
+"""
 function VisibilityTester(p1::Point3, p2::Point3, eps1::Float64, eps2::Float64)
     distance = norm(p2-p1)
     return Ray(p1, (p2-p1)/distance, eps1, distance*(1.0 - eps2), 1)
 end
 
-# VisibilityTester from a point to infinitely far away
+
+"""
+    VisibilityTester(origin::Point3, direction::Vector3, eps::Float64)
+
+Returns a ray to test intersection on the line segment from `origin` infinitely far in the
+given direction, with a given ray epsilon.
+
+"""
 function VisibilityTester(origin::Point3, direction::Vector3, eps::Float64)
     return Ray(origin, direction, eps, Inf, 1)
 end
 
 
 
+
 ################################
 # Point light
 
+
+"""
+    PointLight{S<:Spectrum} <: LightSource
+
+Point light source. It's located at a given point and radiates isotropically. The intensity
+value given is interpreted as the intensity in Watts/m^2 at a distance of 1 meter.
+
+# Fields
+
+- `position::Point3`, the location of the light source.
+- `intensity::S`, the intensity at a distance of 1 meter in Watts/m^2.
+- `light_to_world::Transformation`, transformation to shift the location of the light
+   source.
+- `world_to_light::Transformation`, inverse transformation of the above.
+
+"""
 struct PointLight{S<:Spectrum} <: LightSource
     position::Point3
     intensity::S
@@ -27,14 +57,35 @@ struct PointLight{S<:Spectrum} <: LightSource
     world_to_light::Transformation
 end
 
-# Minimal constructor
+
+"""
+    PointLight(L::Spectrum, l2w::Transformation)
+
+
+Create a new `PointLight` with the given spectrum, located at the origin but shifted by
+the given `Transformation`.
+
+"""
 function PointLight(L::Spectrum, l2w::Transformation)
     PointLight(l2w(Point3(0)), L, l2w, inv(l2w))
 end
 
-isdelta(L::PointLight) = true
-power(L::PointLight) = 4π * L.intensity
 
+"""
+    sample_L(light::PointLight, p::Point3)
+
+Get an intensity sample from the given `DistantLight` and a ray from the light towards the
+given point. The sample is always the same since the light source is a uniform parallel
+light.
+
+Returns:
+
+- Intensity `Spectrum`
+- Direction `Vector3` of the light.
+- A weight coefficient which is always 1.0
+- A ray from the light source to the given point.
+
+"""
 function sample_L(light::PointLight, p::Point3)
     distance = norm(light.position - p)
     L = light.intensity / distance^2
@@ -44,9 +95,33 @@ function sample_L(light::PointLight, p::Point3)
     return L, wi, pdf, vis
 end
 
+
+"""
+    direct(L::PointLight)
+
+Is the given light a direct light source? Returns true for a `PointLight`.
+
+"""
 direct(L::PointLight) = true
+
+
+"""
+    background(L::PointLight)
+
+Give the background contribution of the given light source. Return `nolight` for a
+`PointLight`.
+
+"""
 background(L::PointLight) = nolight
 
+
+"""
+    generate_ray(light::PointLight, bounds::BoundingSphere, S::Sample)
+
+Returns a ray from the light source at the given `BoundingSphere`, parametrized by the
+given random sample.
+
+"""
 function generate_ray(light::PointLight, bounds::BoundingSphere, S::Sample)
     ray = ray_from_point(bounds.radius, S.imgX, S.imgY)
     return light.light_to_world(ray)
@@ -56,24 +131,89 @@ end
 ################################
 # Distanct light source
 
+"""
+    DistantLight{S<:Spectrum} <: LightSource
+    
+A point light source infinitely far away, i.e. parallel rays with a given intensity spectrum
+interpreted as Watts / m^2.
+
+By default the light source is "located" infinitely far away along the positive Z axis and
+the light rays travel in the -Z direction.
+
+# Fields
+
+- `intensity::S`, the intensity of the light in Watts/m^2.
+- `light_to_world::Transformation`, transformation to define the direction of the light
+   source.
+- `world_to_light::Transformation`, inverse transformation of the above.
+
+
+"""
 struct DistantLight{S<:Spectrum} <: LightSource
     intensity::S
     light_to_world::Transformation
     world_to_light::Transformation
 end
 
+
+"""
+    function DistantLight(L::Spectrum, l2w::Transformation)
+
+Create a new `DistantLight` given the intensity spectrum (in units of Watts / m^2) and
+a `Transformation` used to rotate the direction of the light.
+    
+"""
 function DistantLight(L::Spectrum, l2w::Transformation)
     DistantLight(L, l2w, inv(l2w))
 end
 
+
+"""
+    sample_L(light::DistantLight, p::Point3)
+
+Get an intensity sample from the given `DistantLight` and a ray from the light towards the
+given point. The sample is always the same since the light source is a uniform parallel
+light.
+
+Returns:
+
+- Intensity `Spectrum`
+- Direction `Vector3` of the light.
+- A weight coefficient which is always 1.0
+- A ray from the light source to the given point.
+
+"""
 function sample_L(light::DistantLight, p::Point3)
     return light.intensity, NEG_Z_AXIS, 1.0, VisibilityTester(p, NEG_Z_AXIS, 2e-5)
 end
 
+
+"""
+    direct(L::DistantLight)
+
+Is the given light a direct light? Returns `true` for a `DistantLight`.
+
+"""
 direct(L::DistantLight) = true
+
+
+"""
+    background(L::DistantLight)
+
+Returns the background light produced by the lightsource. Returns `nolight` for a
+`DistantLight`.
+
+"""
 background(L::DistantLight) = nolight
 
 
+"""
+    generate_ray(light::DistantLight, bounds::BoundingSphere, S::Sample)
+
+Returns a ray from the light source at the given `BoundingSphere`, parametrized by the
+given random sample.
+
+"""
 function generate_ray(light::DistantLight, bounds::BoundingSphere, S::Sample)
     ray = ray_parallel(bounds.radius, S.imgX, S.imgY)
     return light.light_to_world(ray)
@@ -82,17 +222,51 @@ end
 
 
 
+
 ################################
 # Background light source
 
+"""
+    Background{S<:Spectrum} <: LightSource
+    
+Background light. If one of these is in the scene, every ray that escapes to infinity hits
+this light source.
+
+"""
 struct Background{S<:Spectrum} <: LightSource
     intensity::S
 end
 
+
+"""
+    sample_L(light::Background, p::Point3)
+
+Generate a sample from light source. Return `nolight` because background lights do not
+contribute through this process (they are not direct lights).
+
+"""
 sample_L(light::Background, p::Point3) = nolight
 
+
+"""
+    direct(L::Background)
+
+Is this light source direct? Returns false for `Background` source.
+
+"""
 direct(L::Background) = false
+
+
+"""
+    background(L::Background)
+    
+Returns the background light produced by the lightsource. Returns the intensity of a
+`Background` source.
+
+"""
 background(L::Background) = L.intensity
+
+
 
 
 # TODO: Area light
